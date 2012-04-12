ad49f465816e93d76d1aef278fb1664ec096f9ab
mabrarov <mabrarov@b0109a4c-6076-47d0-973a-9f9728c2f665>
2012-04-12 15:21:55 +0000

    Copyrights updated.

--- src/ma/echo/server/qt/mainform.cpp
+++ src/ma/echo/server/qt/mainform.cpp
@@ -3,7 +3,7 @@ TRANSLATOR ma::echo::server::qt::MainForm
 */
 
 //
-// Copyright (c) 2010-2011 Marat Abrarov (abrarov@mail.ru)
+// Copyright (c) 2010-2012 Marat Abrarov (abrarov@mail.ru)
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
