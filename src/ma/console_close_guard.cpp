0e557a5214841c907880571aae74b46445be3ad2
mabrarov <mabrarov@b0109a4c-6076-47d0-973a-9f9728c2f665>
2013-06-11 06:31:29 +0000

    1. Support for Intel C++ Compiler XE 12.1.x removed. 2. MSVS 2008 projects fixed. 3. MSVS 2010 projects fixed. 4. MSVS 2012 projects for Intel C++ Compiler XE 13.x fixed. 5. Fixed projects not tested (only built).

--- src/ma/console_close_guard.cpp
+++ src/ma/console_close_guard.cpp
@@ -5,21 +5,10 @@
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 
-//#if defined(WIN32)
-//#include <windows.h>
-//#endif
-
-//#if !defined(WIN32)
-//#include <csignal>
-//#endif
-
 #include <csignal>
-//#include <stdexcept>
 #include <boost/bind.hpp>
 #include <boost/asio.hpp>
 #include <boost/thread/thread.hpp>
-//#include <boost/thread/locks.hpp>
-//#include <boost/thread/mutex.hpp>
 #include <ma/console_close_guard.hpp>
 
 namespace {
@@ -119,56 +108,5 @@ console_close_guard::console_close_guard(
 console_close_guard::~console_close_guard()
 {
 }
-
-//#if defined(WIN32)
-//  static BOOL WINAPI win_console_ctrl_routine(DWORD ctrl_type)
-//  {
-//    switch (ctrl_type)
-//    {
-//    case CTRL_C_EVENT:
-//    case CTRL_BREAK_EVENT:
-//    case CTRL_CLOSE_EVENT:
-//    case CTRL_SHUTDOWN_EVENT:
-//    case CTRL_LOGOFF_EVENT:
-//      console_close_guard::get_registry()->handle_console_close();
-//      return FALSE;
-//    default:
-//      return FALSE;
-//    }
-//  }
-//#endif
-
-//console_close_guard::console_close_guard(const ctrl_function_type& crl_function)
-//{
-//  {
-//    lock_guard_type lock(ctrl_mutex_);
-//    if (ctrl_function_)
-//    {
-//      boost::throw_exception(std::logic_error(
-//          "console_close_guard must be the only"));
-//    }
-//    ctrl_function_ = crl_function;
-//  }
-//
-//#if defined(WIN32)
-//  ::SetConsoleCtrlHandler(console_ctrl_proc, TRUE);
-//#else
-//  if (SIG_ERR == ::signal(SIGINT, &console_close_guard::console_ctrl_proc))
-//  {
-//    boost::throw_exception(std::runtime_error(
-//        "failed to set signal handler for SIGQUIT"));
-//  }
-//#endif // defined(WIN32)
-//}
-//
-//console_close_guard::~console_close_guard()
-//{
-//#if defined(WIN32)
-//  ::SetConsoleCtrlHandler(console_ctrl_proc, FALSE);
-//#endif
-//
-//  lock_guard_type lock(ctrl_mutex_);
-//  ctrl_function_.clear();
-//}
 
 } // namespace ma
