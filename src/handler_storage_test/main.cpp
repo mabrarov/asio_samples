6a85b0a71a390ab4a05aef1cdd4595467e937df0
mabrarov <mabrarov@b0109a4c-6076-47d0-973a-9f9728c2f665>
2013-06-17 19:47:44 +0000

    shared_ptr_factory_test project added (MSVS 2012 only).

--- src/handler_storage_test/main.cpp
+++ src/handler_storage_test/main.cpp
@@ -76,20 +76,17 @@ int main(int /*argc*/, char* /*argv*/[])
 #endif
 {
   try
-  {
+  {    
     ma::test::lockable_wrapper::run_test();
     ma::test::handler_storage_service_destruction::run_test();
     ma::test::handler_storage_target::run_test();
     ma::test::handler_storage_arg::run_test();
 
 //    test_handler_storage_arg(io_service);
-//
+
 //#if defined(MA_HAS_RVALUE_REFS)
 //    test_handler_storage_move_constructor(io_service);
 //#endif
-//
-//    test_sp_intrusive_list();
-//    test_shared_ptr_factory();
 
     return EXIT_SUCCESS;
   }
@@ -764,169 +761,3 @@ void run_test()
 
 } // namespace test
 } // namespace ma
-
-//namespace {
-//
-//class later_handler : public test_handler_base
-//{
-//public:
-//  class storage_holder;
-//  typedef boost::shared_ptr<storage_holder> storage_holder_ptr;
-//
-//  explicit later_handler(int value, const storage_holder_ptr& storage_holder)
-//    : value_(value)
-//    , storage_holder_(storage_holder)
-//  {
-//  }
-//
-//  void operator()(int val)
-//  {
-//    std::cout << value_ << val << std::endl;
-//  }
-//
-//  int get_value() const
-//  {
-//    return value_;
-//  }
-//
-//private:
-//  int value_;
-//  storage_holder_ptr storage_holder_;
-//}; // class later_handler
-//
-//class later_handler::storage_holder : private boost::noncopyable
-//{
-//public:
-//  typedef ma::handler_storage<int> handler_storage_type;
-//
-//  explicit storage_holder(boost::asio::io_service& io_service)
-//    : handler_storage_(io_service)
-//  {
-//  }
-//
-//  handler_storage_type& handler_storage()
-//  {
-//    return handler_storage_;
-//  }
-//
-//private:
-//  handler_storage_type handler_storage_;
-//}; // class later_handler::storage_holder
-//
-//void test_handler_storage_arg(boost::asio::io_service& io_service)
-//{
-//
-//}
-//
-//#if defined(MA_HAS_RVALUE_REFS)
-//
-//void test_handler_storage_move_constructor(boost::asio::io_service& io_service)
-//{
-//  std::cout << "*** test_handler_storage_move_constructor ***" << std::endl;
-//
-//  typedef ma::handler_storage<int> handler_storage_type;
-//
-//  handler_storage_type handler1(io_service);
-//  handler1.store(test_handler(4));
-//
-//  handler_storage_type handler2(std::move(handler1));
-//  handler2.post(2);
-//}
-//
-//#endif // defined(MA_HAS_RVALUE_REFS)
-//
-//class sp_list_test : public ma::sp_intrusive_list<sp_list_test>::base_hook
-//{
-//public:
-//  explicit sp_list_test(std::size_t num)
-//    : num_(num)
-//  {
-//  }
-//
-//  ~sp_list_test()
-//  {
-//    std::cout << num_ << std::endl;
-//  }
-//private:
-//  std::size_t num_;
-//}; // sp_list_test
-//
-//void test_sp_intrusive_list()
-//{
-//  std::cout << "*** test_sp_intrusive_list ***" << std::endl;
-//  ma::sp_intrusive_list<sp_list_test> sp_list;
-//  for (std::size_t i = 0; i != 10; ++i)
-//  {
-//    std::cout << i << std::endl;
-//    sp_list.push_front(boost::make_shared<sp_list_test>(i));
-//  }
-//}
-//
-//class A : private boost::noncopyable
-//{
-//public:
-//  static boost::shared_ptr<A> create()
-//  {
-//    typedef ma::shared_ptr_factory_helper<A> shared_ptr_helper;
-//    return boost::make_shared<shared_ptr_helper>();
-//  }
-//
-//protected:
-//  ~A()
-//  {
-//  }
-//};
-//
-//class B : private boost::noncopyable
-//{
-//public:
-//  static boost::shared_ptr<B> create(int data)
-//  {
-//    typedef ma::shared_ptr_factory_helper<B> shared_ptr_helper;
-//    return boost::make_shared<shared_ptr_helper>(data);
-//  }
-//
-//protected:
-//  explicit B(int /*data*/)
-//  {
-//  }
-//
-//  ~B()
-//  {
-//  }
-//};
-//
-//void test_shared_ptr_factory()
-//{
-//  std::cout << "*** test_shared_ptr_factory ***" << std::endl;
-//  boost::shared_ptr<const A> a = A::create();
-//  boost::shared_ptr<B> b = B::create(42);
-//}
-//
-//class io_service_pool : private boost::noncopyable
-//{
-//public:
-//  io_service_pool(boost::asio::io_service& io_service, std::size_t size)
-//    : work_guard_(boost::in_place(boost::ref(io_service)))
-//  {
-//    for (std::size_t i = 0; i < size; ++i)
-//    {
-//      threads_.create_thread(
-//        boost::bind(&boost::asio::io_service::run, &io_service));
-//    }
-//  }
-//
-//  ~io_service_pool()
-//  {
-//    work_guard_ = boost::none;
-//    threads_.join_all();
-//  }
-//
-//private:
-//  typedef boost::optional<boost::asio::io_service::work> optional_io_work;
-//
-//  optional_io_work    work_guard_;
-//  boost::thread_group threads_;
-//}; // class io_service_pool
-//
-//} // anonymous namespace
