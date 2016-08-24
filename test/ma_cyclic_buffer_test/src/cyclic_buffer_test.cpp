9f05f9fd473c5cd4811a4869201f3ed676d0baaa
Marat Abrarov <abrarov@gmail.com>
2016-08-24 22:35:25 +0300

    Wireframe for test for ma::cyclic_buffer class.

---
+++ test/ma_cyclic_buffer_test/src/cyclic_buffer_test.cpp
@@ -0,0 +1,22 @@
+//
+// Copyright (c) 2016 Marat Abrarov (abrarov@gmail.com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include <gtest/gtest.h>
+#include <ma/cyclic_buffer.hpp>
+
+namespace ma {
+namespace test {
+namespace cyclic_buffer_test {
+
+TEST(cyclic_buffer, simple)
+{
+  //todo
+}
+
+} // namespace cyclic_buffer_test
+} // namespace test
+} // namespace ma
