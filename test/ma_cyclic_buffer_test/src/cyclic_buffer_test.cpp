e04e97c43113562b3d74e06782418886cd1ceac2
Marat Abrarov <abrarov@gmail.com>
2016-08-25 11:40:28 +0300

    Added simple tests for ma::cyclic_buffer with zero size.

--- test/ma_cyclic_buffer_test/src/cyclic_buffer_test.cpp
+++ test/ma_cyclic_buffer_test/src/cyclic_buffer_test.cpp
@@ -5,6 +5,7 @@
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 
+#include <boost/asio.hpp>
 #include <gtest/gtest.h>
 #include <ma/cyclic_buffer.hpp>
 
@@ -12,9 +13,20 @@ namespace ma {
 namespace test {
 namespace cyclic_buffer_test {
 
-TEST(cyclic_buffer, simple)
+TEST(zero_size_cyclic_buffer, no_free_space)
 {
-  //todo
+  typedef ma::cyclic_buffer::mutable_buffers_type mutable_buffers_type;
+  ma::cyclic_buffer test_buffer(0);
+  mutable_buffers_type free_space = test_buffer.prepared();
+  ASSERT_EQ(0U, boost::asio::buffer_size(free_space));
+}
+
+TEST(zero_size_cyclic_buffer, no_filled_space)
+{
+  typedef ma::cyclic_buffer::const_buffers_type const_buffers_type;
+  ma::cyclic_buffer test_buffer(0);
+  const_buffers_type filled_space = test_buffer.data();
+  ASSERT_EQ(0U, boost::asio::buffer_size(filled_space));
 }
 
 } // namespace cyclic_buffer_test
