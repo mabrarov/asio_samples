c79ee1a7e1937f04eb60f223460759d82dcac9e8
Marat Abrarov <abrarov@gmail.com>
2018-04-12 22:11:13 +0300

    Minor renaming.

--- libs/ma_helpers/include/ma/io_context_helpers.hpp
+++ libs/ma_helpers/include/ma/io_context_helpers.hpp
@@ -5,8 +5,8 @@
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 
-#ifndef MA_IO_CONTEXT_INVOKE_HELPERS_HPP
-#define MA_IO_CONTEXT_INVOKE_HELPERS_HPP
+#ifndef MA_IO_CONTEXT_HELPERS_HPP
+#define MA_IO_CONTEXT_HELPERS_HPP
 
 #if defined(_MSC_VER) && (_MSC_VER >= 1020)
 #pragma once
@@ -43,4 +43,4 @@ to_io_context_concurrency_hint(std::size_t hint)
 
 } // namespace ma
 
-#endif // MA_IO_CONTEXT_INVOKE_HELPERS_HPP
+#endif // MA_IO_CONTEXT_HELPERS_HPP
