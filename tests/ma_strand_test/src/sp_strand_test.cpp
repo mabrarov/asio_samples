fa014286641a42a84cd5c16ff6a1cd6f8ff99a54
Marat Abrarov <abrarov@gmail.com>
2018-05-31 22:36:47 +0300

    Wireframe for tests for ma::strand.

---
+++ tests/ma_strand_test/src/sp_strand_test.cpp
@@ -0,0 +1,22 @@
+//
+// Copyright (c) 2018 Marat Abrarov (abrarov@gmail.com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include <cstddef>
+#include <boost/noncopyable.hpp>
+#include <gtest/gtest.h>
+#include <ma/strand.hpp>
+#include <ma/detail/functional.hpp>
+#include <ma/detail/memory.hpp>
+
+namespace ma {
+namespace test {
+namespace strand {
+
+
+} // namespace strand
+} // namespace test
+} // namespace ma
