62d048de7a7a413faa64e9dc6da70fa0ff873862
Marat Abrarov <abrarov@gmail.com>
2019-01-17 09:44:20 +0300

    Migrated docker images tll teh latest versions of Alpine Linux and Ubuntu, fixed CMake parameters (RPATH), fixed syntax in shell scripts.

--- docker/builder/centos/start.sh
+++ docker/builder/centos/start.sh
@@ -4,7 +4,8 @@
 set -e
 
 # Generate Makefile project
-cmake -D CMAKE_BUILD_TYPE=${BUILD_TYPE} \
+cmake -D CMAKE_SKIP_BUILD_RPATH=ON \
+      -D CMAKE_BUILD_TYPE=${BUILD_TYPE} \
       -D Boost_USE_STATIC_LIBS=${BOOST_USE_STATIC_LIBS} \
       -D ma_qt=${MA_QT} -D ma_force_qt_major_version=${MA_QT_MAJOR_VERSION} \
       -D ma_coverage_build=${COVERAGE_BUILD} \
@@ -15,7 +16,7 @@ cmake -D CMAKE_BUILD_TYPE=${BUILD_TYPE} \
 cmake --build . --config ${BUILD_TYPE}
 
 # Prepare zero counters - baseline - for code coverage calculation
-if [ "${COVERAGE_BUILD}" = "ON" ]; then
+if [[ "${COVERAGE_BUILD}" = "ON" ]]; then
     lcov -z -d . &&\
     lcov -c -d . -i -o lcov-base.info;
 fi
@@ -24,7 +25,7 @@ fi
 ctest --build-config ${BUILD_TYPE} --verbose
 
 # Calculate difference to get code coverage statistic and generate HTML report
-if [ "${COVERAGE_BUILD}" = "ON" ]; then
+if [[ "${COVERAGE_BUILD}" = "ON" ]]; then
     lcov -c -d . -o lcov-test.info &&\
     lcov -a lcov-base.info -a lcov-test.info -o lcov.info &&\
     lcov -r lcov.info "$(pwd)/**/ui_*.h*" "$(pwd)/**/moc_*.c*" "/usr/*" "${PROJECT_DIR}/3rdparty/*" "${PROJECT_DIR}/examples/*" "${PROJECT_DIR}/tests/*" -o lcov.info &&\
