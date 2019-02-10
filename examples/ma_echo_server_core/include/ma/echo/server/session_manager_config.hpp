0a8d031acec476137d3befe069e8f6f976aca51b
Marat Abrarov <abrarov@gmail.com>
2019-02-10 23:12:06 +0300

    Fixed reference to license in copyright headers

--- examples/ma_echo_server_core/include/ma/echo/server/session_manager_config.hpp
+++ examples/ma_echo_server_core/include/ma/echo/server/session_manager_config.hpp
@@ -2,7 +2,7 @@
 // Copyright (c) 2010-2015 Marat Abrarov (abrarov@gmail.com)
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
-// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+// file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 
 #ifndef MA_ECHO_SERVER_SESSION_MANAGER_CONFIG_HPP
