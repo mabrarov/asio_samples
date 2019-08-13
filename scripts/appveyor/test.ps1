cf8981aa666090877e1be7dc9d032e11e9b3b18b
Marat Abrarov <abrarov@gmail.com>
2019-08-13 23:50:56 +0300

    Temporary fix for Codecov in AppVeyor CI

--- scripts/appveyor/test.ps1
+++ scripts/appveyor/test.ps1
@@ -29,7 +29,7 @@ if (${env:COVERAGE_BUILD} -eq "True") {
   Push-AppveyorArtifact "${coverage_report_archive}" -DeploymentName "${env:COVERAGE_ARTIFACT_NAME}"
   Push-AppveyorArtifact "${env:COBERTURA_COVERAGE_FILE}" -DeploymentName "${env:COVERAGE_ARTIFACT_NAME}"
 
-  Write-Host "Calling: codecov --required --token ""${env:CODECOV_TOKEN}"" --file ""${env:COBERTURA_COVERAGE_FILE}"" --flags ""${env:CODECOV_FLAG}"" -X gcov --root ""${env:APPVEYOR_BUILD_FOLDER}""
+  Write-Host "Calling: codecov --required --token ""${env:CODECOV_TOKEN}"" --file ""${env:COBERTURA_COVERAGE_FILE}"" --flags ""${env:CODECOV_FLAG}"" -X gcov --root ""${env:APPVEYOR_BUILD_FOLDER}"""
   codecov --required --token "${env:CODECOV_TOKEN}" --file "${env:COBERTURA_COVERAGE_FILE}" --flags "${env:CODECOV_FLAG}" -X gcov --root "${env:APPVEYOR_BUILD_FOLDER}"
   if (${LastExitCode} -ne 0) {
     throw "Codecov failed with exit code ${LastExitCode}"
