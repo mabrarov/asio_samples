9d00be8c3beb3affe3deb1cb6dc99d6cd5201da6
Marat Abrarov <abrarov@gmail.com>
2019-08-14 17:25:17 +0300

    Copied travis_retry function into Travis script to fix "command not found" error, because travis_retry function cannot be used in sub shell

--- scripts/travis/build.sh
+++ scripts/travis/build.sh
@@ -9,24 +9,8 @@
 
 set -e
 
-travis_retry() {
-  local result=0
-  local count=1
-  while [[ "${count}" -le 3 ]]; do
-    [[ "${result}" -ne 0 ]] && {
-      echo -e "\\n${ANSI_RED}The command \"${*}\" failed. Retrying, ${count} of 3.${ANSI_RESET}\\n" >&2
-    }
-    "${@}" && { result=0 && break; } || result="${?}"
-    count="$((count + 1))"
-    sleep 1
-  done
-
-  [[ "${count}" -gt 3 ]] && {
-    echo -e "\\n${ANSI_RED}The command \"${*}\" failed 3 times.${ANSI_RESET}\\n" >&2
-  }
-
-  return "${result}"
-}
+this_path="$(cd "$(dirname "$0")" && pwd)"
+source "${this_path}/travis_retry.sh"
 
 codecov_flag="${TRAVIS_OS_NAME}__$(uname -r | sed -r 's/[[:space:]]|[\\\.\/:]/_/g')__${CXX_COMPILER_FAMILY}_$(${CXX_COMPILER} -dumpversion)__boost_${BOOST_VERSION}__qt_${QT_MAJOR_VERSION}"
 codecov_flag="${codecov_flag//[.-]/_}"
