0125e6b6f63dca40df948a978c7fc40bf6c3e0f1
Marat Abrarov <abrarov@gmail.com>
2020-08-24 12:58:59 +0300

    Better support of null pointers in owns methods of custom allocators

--- libs/ma_custom_alloc_handler/include/ma/handler_allocator.hpp
+++ libs/ma_custom_alloc_handler/include/ma/handler_allocator.hpp
@@ -138,6 +138,10 @@ void in_place_handler_allocator<alloc_size>::deallocate(void* pointer)
 template <std::size_t alloc_size>
 bool in_place_handler_allocator<alloc_size>::owns(void* pointer) const
 {
+  if (!pointer)
+  {
+    return false;
+  }
   const byte_type* begin = static_cast<const byte_type*>(storage_.address());
   const byte_type* end = begin + alloc_size;
   const byte_type* p = static_cast<const byte_type*>(pointer);
@@ -199,12 +203,20 @@ inline void in_heap_handler_allocator::deallocate(void* pointer)
 
 inline bool in_heap_handler_allocator::owns(void* pointer) const
 {
+  if (!pointer)
+  {
+    return false;
+  }
   const byte_type* begin = storage_.get();
+  if (!begin)
+  {
+    return false;
+  }
   const byte_type* end = begin + size_;
   const byte_type* p = static_cast<const byte_type*>(pointer);
   //fixme: Undefined behavior - comparison of unrelated pointers
   // thanks to https://www.viva64.com/en/b/0576/
-  return (p >= begin) && (p < end) && begin && p;
+  return (p >= begin) && (p < end);
 }
 
 inline std::size_t in_heap_handler_allocator::size() const
