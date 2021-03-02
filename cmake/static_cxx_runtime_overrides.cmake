18f5562452464609020cf183fdce526e55f88cdf
Marat Abrarov <abrarov@gmail.com>
2021-03-02 21:01:20 +0300

    Fixed GCC linker flags for static C/C++ runtime linkage.

--- cmake/static_cxx_runtime_overrides.cmake
+++ cmake/static_cxx_runtime_overrides.cmake
@@ -18,6 +18,8 @@ endif()
 
 if(CMAKE_COMPILER_IS_GNUCC)
     set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -static -static-libgcc -static-libstdc++")
+    set(CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS} -static -static-libgcc -static-libstdc++")
+    set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -static -static-libgcc -static-libstdc++")
 endif()
 
 set(Boost_USE_STATIC_LIBS    ON)
